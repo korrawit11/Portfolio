bf62bfa96822b4e816fbec3423e0da54d354a6d5
korrawit11 <korrawit081@gmail.com>
2022-08-31 12:49:16 +0700

    add image project preview

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,6 +2,10 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
+import Colmar from "../../Assets/Projects/colmar.png"
+import Jamming from "../../Assets/Projects/jamming.png"
+import Fitvenger from "../../Assets/Projects/fitvenger.png"
+
 
 function Projects() {
   return (
@@ -17,14 +21,17 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
+              imgPath = {Fitvenger}
               title="FIT-VENGER"
               description="This project about track or keep your activities to website. We use the CSS, JavaScript, ReactJs, MongoDB to build this website."
               ghLink="https://github.com/korrawit11/fitvenger"
+              demoLink="https://fitvenger-git-expresssessiondeployment-kiatbordin.vercel.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
+              imgPath = {Jamming}
               title="Jamming"
               description="One of the project about API. I study about react and create this website from react and use API to save playlist to Spotify and anybody who have I access to use it they can use my playlist in Spotify application."
               ghLink="https://github.com/korrawit11/jamming"
@@ -33,6 +40,7 @@ function Projects() {
           </Col>
           <Col md={4} className="project-card">
             <ProjectCard
+              imgPath = {Colmar}
               title="Colmar Academy"
               description="This is my first project with HTML and CSS. This was hard for me to start but I can pass it. I've learn a lot!"
               ghLink="https://github.com/korrawit11/Colmar-Academy"
